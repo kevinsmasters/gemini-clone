b60cf30a68a2f762241f91ab203e7de247e3f093
Kevin Masters <irimiuke@gmail.com>
2024-10-03 21:36:54 -0400

    added main cards and css

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -11,6 +11,25 @@ const Main = () => {
       <div className="main-container">
         <div className="greet">
           <p><span>Meowdy, Dev.</span></p>
+          <p>How can I help you today?</p>
+        </div>
+        <div className="cards">
+          <div className="card">
+            <p>Suggest beautiful places to see on an upcoming road trip</p>
+            <img src={assets.compass_icon} alt="" />
+          </div>
+          <div className="card">
+            <p>Briefly summarize this concept: urban planning</p>
+            <img src={assets.bulb_icon} alt="" />
+          </div>
+          <div className="card">
+            <p>Brainstorm team bonding exercises for our work retreat</p>
+            <img src={assets.message_icon} alt="" />
+          </div>
+          <div className="card">
+            <p>Improve the readibility of the following code</p>
+            <img src={assets.code_icon} alt="" />
+          </div>
         </div>
       </div>
     </div>
