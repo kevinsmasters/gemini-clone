04d14bd6cac94b2bfcca0f1d841c4945fce57457
Kevin Masters <irimiuke@gmail.com>
2024-10-14 20:04:16 -0400

    delay word-for-word to simulate typing response

--- src/context/Context.jsx
+++ src/context/Context.jsx
@@ -12,7 +12,11 @@ const ContextProvider = (props) => {
   const [loading,setLoading] = useState(false);
   const [resultData,setResultData] = useState("")
 
-  const delayPara = (index,nextWord) => {}
+  const delayPara = (index,nextWord) => {
+    setTimeout(function () {
+      setResultData(prev=>prev+nextWord)
+    }, 75*index)
+  }
 
   const onSent = async (prompt) => {
 
@@ -31,7 +35,11 @@ const ContextProvider = (props) => {
       }
     }
     let newResponse2 = newResponse.split("*").join("<br>")
-    setResultData(newResponse2);
+    let newResponseArray = newResponse2.split(" ");
+    for(let i = 0; i < newResponseArray.length; i ++) {
+      const nextWord = newResponseArray[i]
+      delayPara(i, nextWord+" ");
+    }
     setLoading(false);
     setInput("");
     
