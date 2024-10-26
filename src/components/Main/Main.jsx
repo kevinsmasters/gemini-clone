42208bb6ba84bdbcfe4941de793250e8f273870f
Kevin Masters <irimiuke@gmail.com>
2024-10-25 20:43:39 -0400

    submit on pressing enter key

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -74,7 +74,17 @@ const Main = () => {
         
         <div className="main-bottom">
           <div className="search-box">
-            <input onChange={(e)=> setInput(e.target.value)} value={input} type="text" placeholder='Enter a prompt here' />
+            <input 
+            onChange={(e)=> setInput(e.target.value)} 
+            value={input} 
+            type="text" 
+            placeholder='Enter a prompt here' 
+            onKeyDown={event => {
+              if(event.key === 'Enter') {
+                onSent()
+              }
+            }}
+            />
             <div>
               <img src={assets.gallery_icon} alt="" />
               <img src={assets.mic_icon} alt="" />
